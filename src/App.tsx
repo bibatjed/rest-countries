109a1e6acd2a72a7f31f11baeed8c6783d2cd43c
Jeeed <43069218+bibatjed@users.noreply.github.com>
2022-12-09 15:44:14 +0800

    Search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChangeEvent, useEffect, useState } from "react";
+import "./App.css";
 
+type Country = {
+  name: {
+    official: string;
+  };
+};
 function App() {
+  const [data, setData] = useState<Country[] | null>(null);
+  const [search, setSearch] = useState("");
+  const [selected, setSelected] = useState("");
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((data) => data.json())
+      .then((data) => setData(data));
+  }, []);
+
+  useEffect(() => {
+    if (selected) {
+      console.log("hello");
+      fetch(`https://restcountries.com/v3.1/region/${selected}`)
+        .then((data) => data.json())
+        .then((data) => {
+          setData(data);
+        });
+    }
+  }, [selected]);
+
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+  const handleOnChangeSelect = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSelected(e.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={handleOnChange} /> search for country
+      <select value={selected} onChange={handleOnChangeSelect}>
+        <option disabled={true} value="">
+          choose
+        </option>
+        <option value="africa">africa</option>
+        <option value="europe">europe</option>
+        <option value="asia">asia</option>
+        <option value="america">america</option>
+        <option value="oceania">oceania</option>
+      </select>
+      {JSON.stringify(
+        data?.filter((value) => {
+          if (
+            value.name.official.toLowerCase().search(search.toLowerCase()) > -1
+          ) {
+            return value;
+          }
+        })
+      )}
     </div>
   );
 }
