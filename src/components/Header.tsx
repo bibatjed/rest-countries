d4b277897a89466a92d1b13707540e986c0edd5c
Jeeed <43069218+bibatjed@users.noreply.github.com>
2022-12-12 14:15:56 +0800

    Add header with toggle theme functionality

---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { MdDarkMode, MdOutlineDarkMode } from "react-icons/md";
+import { useLocalStorage } from "usehooks-ts";
+export default function Header() {
+  const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [theme, setTheme] = useLocalStorage(
+    "theme",
+    defaultDark ? "dark" : "light"
+  );
+  const handleChangeTheme = () => {
+    setTheme((prevTheme) => {
+      return prevTheme === "dark" ? "light" : "dark";
+    });
+  };
+  useEffect(() => {
+    if (theme === "dark") {
+      document.body.classList.add("dark");
+      document.body.classList.remove("light");
+    } else {
+      document.body.classList.add("light");
+      document.body.classList.remove("dark");
+    }
+  }, [theme]);
+  return (
+    <div className="p-5 dark:bg-s-dm-dark-blue flex justify-between items-center bg-s-white border-b-2 border-b-s-lm-very-light-gray">
+      <h1 className="text-base dark:text-s-lm-very-light-gray font-nunito-sans font-extrabold">
+        Where in the world?
+      </h1>
+      <button onClick={handleChangeTheme} className="flex items-center gap-2">
+        <MdDarkMode className="dark:text-s-lm-very-light-gray" />
+        <span className="text-sm dark:text-s-lm-very-light-gray font-nunito-sans font-semibold">
+          Dark Mode
+        </span>
+      </button>
+    </div>
+  );
+}
