16cb15b55be28a15efd3bd5fae2b055f7027d05a
Jeeed <43069218+bibatjed@users.noreply.github.com>
2022-12-14 12:26:20 +0800

    Use main component to reuse header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { ChangeEvent } from "react";
 import Country from "./components/Country";
-import Header from "./components/Header";
+import Main from "./components/Main";
 import Search from "./components/Search";
 import Select from "./components/Select";
 import useCountries from "./hooks/useCountries";
@@ -33,30 +33,31 @@ function App() {
     setSearch(e.target.value);
   };
   return (
-    <div className="bg-s-lm-very-light-gray dark:bg-s-dm-very-dark-blue h-full min-h-screen">
-      <Header />
-      <div className="w-full flex-col flex items-center">
-        <div className="flex md:flex-row md:justify-between md:mt-12 flex-col w-11/12 gap-8 mt-6">
-          <Search setSearch={handleOnChange} />
-          <Select SelectOptions={selectOptions} setSelected={setSelected} />
-        </div>
-        <div className="w-11/12 m-auto mt-9 md:mt-16 grid grid-cols-225 xxl:grid-cols-4 gap-4">
-          {dataFilter?.map((value) => {
-            return (
-              <div>
-                <Country
-                  image={value.flags.png}
-                  name={value.name.common}
-                  population={value.population}
-                  region={value.region}
-                  capital={value.capital}
-                />
-              </div>
-            );
-          })}
+    <Main>
+      <div className="bg-s-lm-very-light-gray dark:bg-s-dm-very-dark-blue h-full min-h-screen">
+        <div className="w-full flex-col flex items-center">
+          <div className="flex md:flex-row md:justify-between md:mt-12 flex-col w-11/12 gap-8 mt-6">
+            <Search setSearch={handleOnChange} />
+            <Select SelectOptions={selectOptions} setSelected={setSelected} />
+          </div>
+          <div className="w-11/12 m-auto mt-9 md:mt-16 grid grid-cols-225 xxl:grid-cols-4 gap-4">
+            {dataFilter?.map((value) => {
+              return (
+                <>
+                  <Country
+                    image={value.flags.png}
+                    name={value.name.common}
+                    population={value.population}
+                    region={value.region}
+                    capital={value.capital}
+                  />
+                </>
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
+    </Main>
   );
 }
 
